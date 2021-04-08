ad9d1c9f36b999542c9419a58fdde5edb3c81957
Karson Costner <kccoug44@gmail.com>
2021-04-08 14:39:33 -0600

    Complete

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import promiseMiddleware from 'redux-promise-middleware';
-import budgetReducer from './budgetReducer';
-import userReducer from './userReducer';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import promiseMiddleware from "redux-promise-middleware";
+import budgetReducer from "./budgetReducer";
+import userReducer from "./userReducer";
 
-	
 const rootReducer = combineReducers({
   budget: budgetReducer,
-  user: userReducer
-})
+  user: userReducer,
+});
 
 export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
+
+// Chicken sandwiches are amazing
